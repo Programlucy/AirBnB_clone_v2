770aae0fb97c2966e26d268a5507c25033ba7343
Programlucy <program.testing577@gmail.com>
2023-07-09 16:49:38 +0000

    Airbnb correction

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,33 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        if kwargs:
             from models import storage
+            for key, value in kwargs.items():
+                if key.lower() == "created_at" or key.lower() == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -30,15 +38,24 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
+        storage.new(self)
         self.updated_at = datetime.now()
         storage.save()
+
+    def delete(self):
+        """Deletes the current instance from storage"""
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        for attr_name, attr_value in self.__dict__.items():
+            if attr_name == '_sa_instance_state':
+                continue
+            if isinstance(attr_value.__class__, InstrumentedAttribute):
+                attr_value = getattr(self, attr_name)
+            if isinstance(attr_value, datetime):
+                attr_value = attr_value.isoformat()
+            dictionary[attr_name] = attr_value
+        dictionary['__class__'] = type(self).__name__
         return dictionary
